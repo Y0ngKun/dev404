da3f3a7562fe2763c15adafe679de522c418de7a
Y0ngKun <cksm00@naver.com>
2023-05-16 00:14:47 +0900

    Add files via upload

---
+++ src/main/java/org/ezen/ex02/util/JScript.java
@@ -0,0 +1,23 @@
+package org.ezen.ex02.util;
+
+public class JScript {
+	
+	public static String href(String alertMessage, String locationPath) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<script>");
+		sb.append("alert('"+ alertMessage + "');");
+		sb.append("location.href='"+locationPath+"';"); //특정 이동 경로로 요청
+		sb.append("</script>");
+		return sb.toString();
+		
+	}
+	
+	public static String back(String alertMessage) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<script>");
+		sb.append("alert('"+ alertMessage + "');");
+		sb.append("history.back();"); //뒤로가기
+		sb.append("</script>");
+		return sb.toString();
+	}
+}
